7b6856cef7dfdc7662952ee83efbd7333cd418fe
hrtwt <29752757+hrtwt@users.noreply.github.com>
2022-02-21 17:56:11 +0900

    refactor

--- backend/src/main/java/jp/kusumotolab/fldemo/controller/HealthController.java
+++ backend/src/main/java/jp/kusumotolab/fldemo/controller/HealthController.java
@@ -1,13 +1,15 @@
 package jp.kusumotolab.fldemo.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("health")
 public class HealthController {
 
-  @RequestMapping("/health")
+  @GetMapping
   public void health() {
     //returns 200 OK
   }
