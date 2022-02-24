14aefc0d60ed95ab416ea6e99853b9189b968512
r-takaic <r-takaic@ist.osaka-u.ac.jp>
2022-02-24 20:56:00 +0900

    無駄な再レンダリングを開発中に検出するやつを導入した

---
+++ frontend/debug/whyDidYouRender.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import whyDidYouRender from '@welldone-software/why-did-you-render';
+
+whyDidYouRender(React);
+console.log('start why-did-you-render')
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, {dev, isServer, defaultLoaders}) => {
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
       ignored: /node_modules/,
     }
+
+    if(dev && !isServer) {
+      const originalEntry = config.entry;
+      config.entry = async () => {
+        const entries = await originalEntry();
+        if (entries['main.js'] && !entries['main.js'].includes('./debug/whyDidYouRender.js')) {
+          entries['main.js'].unshift('./debug/whyDidYouRender.js');
+        }
+        return entries;
+      };
+    }
+
     return config
   },
   async rewrites() {
