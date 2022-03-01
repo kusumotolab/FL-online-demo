34545df0fcd0bfe7aa033e6cfd4596d821447a8a
hrtwt <29752757+hrtwt@users.noreply.github.com>
2022-03-02 03:24:21 +0900

    impl get api/fl/technics

--- backend/src/main/java/jp/kusumotolab/fldemo/controller/FlController.java
+++ backend/src/main/java/jp/kusumotolab/fldemo/controller/FlController.java
@@ -1,12 +1,16 @@
 package jp.kusumotolab.fldemo.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import jp.kusumotolab.fldemo.common.FlKind;
 import jp.kusumotolab.fldemo.data.FlResult;
 import jp.kusumotolab.fldemo.data.SrcAndTests;
 import jp.kusumotolab.fldemo.service.KgenprogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +28,10 @@ public class FlController {
   @ResponseBody
   public Map<String, FlResult> flAll(@Validated @RequestBody SrcAndTests st) {
     return kgenprogService.execFl(st);
+  }
+
+  @GetMapping("technics")
+  public List<FlKind> technics() {
+    return Arrays.asList(FlKind.values());
   }
 }
