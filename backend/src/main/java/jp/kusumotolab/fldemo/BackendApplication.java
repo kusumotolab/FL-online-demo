c8e3587af022193c56eaf22661f0e48965078dd0
hrtwt <29752757+hrtwt@users.noreply.github.com>
2022-03-02 12:50:40 +0900

    enable openAPI

--- backend/src/main/java/jp/kusumotolab/fldemo/BackendApplication.java
+++ backend/src/main/java/jp/kusumotolab/fldemo/BackendApplication.java
@@ -1,9 +1,12 @@
 package jp.kusumotolab.fldemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "FL-Online-Demo"))
 public class BackendApplication {
 
   public static void main(String[] args) {
