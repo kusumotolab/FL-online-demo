e6d86f870565214fe5c1e1cee70f6a582d3b0c65
hrtwt <29752757+hrtwt@users.noreply.github.com>
2022-03-02 12:53:59 +0900

    add openapi for health controller

--- backend/src/main/java/jp/kusumotolab/fldemo/controller/HealthController.java
+++ backend/src/main/java/jp/kusumotolab/fldemo/controller/HealthController.java
@@ -1,11 +1,13 @@
 package jp.kusumotolab.fldemo.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/health")
+@Tag(name = "Health", description = "For alive monitoring")
 public class HealthController {
 
   @GetMapping
