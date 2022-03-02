70c7cd067c7fc050b266522d143f0abb3582fcb8
hrtwt <29752757+hrtwt@users.noreply.github.com>
2022-03-02 13:06:20 +0900

    add openapi for fl controller

--- backend/src/main/java/jp/kusumotolab/fldemo/controller/FlController.java
+++ backend/src/main/java/jp/kusumotolab/fldemo/controller/FlController.java
@@ -1,5 +1,9 @@
 package jp.kusumotolab.fldemo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -14,22 +18,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/fl")
 @Scope("request")
+@Tag(name = "FaultLocalization")
 public class FlController {
 
   @Autowired private KgenprogService kgenprogService;
 
+  @Operation(summary = "Exec FaultLocalization")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "Succeeded"),
+        @ApiResponse(responseCode = "400", description = "Validation Error")
+      })
   @PostMapping("all")
-  @ResponseBody
   public Map<String, FlResult> flAll(@Validated @RequestBody SrcAndTests st) {
     return kgenprogService.execFl(st);
   }
 
+  @Operation(
+      summary = "List FaultLocalization Technics",
+      description = "List of all available faultLocalization technics")
+  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Succeeded")})
   @GetMapping("technics")
   public List<FlKind> technics() {
     return Arrays.asList(FlKind.values());
