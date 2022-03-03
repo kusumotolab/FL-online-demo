bc689bae57bf68f6db1a460e674f7783877cd264
hrtwt <29752757+hrtwt@users.noreply.github.com>
2022-03-03 13:37:29 +0900

    add doc to TestController

--- backend/src/main/java/jp/kusumotolab/fldemo/controller/TestController.java
+++ backend/src/main/java/jp/kusumotolab/fldemo/controller/TestController.java
@@ -1,5 +1,9 @@
 package jp.kusumotolab.fldemo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import jp.kusumotolab.fldemo.data.SrcAndTests;
 import jp.kusumotolab.fldemo.data.TestResultWithCoverage;
@@ -9,18 +13,24 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/test")
 @Scope("request")
+@Tag(name = "Test")
 public class TestController {
 
-  @Autowired private KgenprogService kgenprogService;
+  @Autowired
+  private KgenprogService kgenprogService;
 
+  @Operation(summary = "Exec Test")
+  @ApiResponses(
+      value = {
+          @ApiResponse(responseCode = "200", description = "Succeeded"),
+          @ApiResponse(responseCode = "400", description = "Validation Error")
+      })
   @PostMapping
-  @ResponseBody
   public List<TestResultWithCoverage> execTests(@RequestBody SrcAndTests st) {
     return kgenprogService.execTests(st);
   }
