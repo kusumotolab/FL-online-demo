11ec905f6fdc20e88359efcec72433c27722be70
hrtwt <29752757+hrtwt@users.noreply.github.com>
2023-01-01 20:51:35 +0900

    change required to requiredMode because required is deprecated

--- backend/src/main/java/jp/kusumotolab/fldemo/data/Coverage.java
+++ backend/src/main/java/jp/kusumotolab/fldemo/data/Coverage.java
@@ -2,13 +2,15 @@ package jp.kusumotolab.fldemo.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 
 @Schema(description = "Coverage")
 public record Coverage(
-    @Schema(required = true, description = "line number", example = "1") @JsonProperty("lineNumber")
+    @Schema(requiredMode = RequiredMode.REQUIRED, description = "line number", example = "1")
+        @JsonProperty("lineNumber")
         int lineNumber,
     @Schema(
-            required = true,
+            requiredMode = RequiredMode.REQUIRED,
             description = "status of coverage. The value is EMPTY, COVERED, or NOT_COVERED.",
             example = "COVERED")
         @JsonProperty("status")
--- backend/src/main/java/jp/kusumotolab/fldemo/data/SrcDTO.java
+++ backend/src/main/java/jp/kusumotolab/fldemo/data/SrcDTO.java
@@ -2,6 +2,7 @@ package jp.kusumotolab.fldemo.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,7 +12,7 @@ import javax.validation.constraints.NotBlank;
 @Schema(description = "source and tests to exec")
 public record SrcDTO(
     @Schema(
-            required = true,
+            requiredMode = RequiredMode.REQUIRED,
             description = "source code to exec",
             example =
                 "package example;\n\npublic class CloseToZero {\n\n  /* Increase or reduce the given value to be close to zero. */\n  public static int run(int n) {\n    if (n == 0) {\n      n++; // a bug here. zero should be zero\n    } else if (n > 0) {\n      n--;\n    } else {\n      n++;\n    }\n    return n;\n  }\n\n}")
@@ -19,7 +20,7 @@ public record SrcDTO(
         @JsonProperty("src")
         String src,
     @Schema(
-            required = true,
+            requiredMode = RequiredMode.REQUIRED,
             description = "test code to exec",
             example =
                 "package example;\n\nimport static org.junit.Assert.assertEquals;\nimport org.junit.Test;\n\npublic class CloseToZeroTest {\n  @Test\n  public void test01() {\n    assertEquals(4, CloseToZero.run(5));\n  }\n\n  @Test\n  public void test02() {\n    assertEquals(0, CloseToZero.run(0));\n  }\n\n  @Test\n  public void test03() {\n    assertEquals(-4, CloseToZero.run(-5));\n  }\n\n}")
